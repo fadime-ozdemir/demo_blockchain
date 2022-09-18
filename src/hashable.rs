a11fd4a40d44c2a9d989adfb061facf2a53a3e5f
fadime <fadime.ozdemir11@gmail.com>
2022-09-18 18:36:57 +0300

    add transactions

--- src/hashable.rs
+++ src/hashable.rs
@@ -1,6 +1,7 @@
+use super::*;
 pub trait Hashable {
     fn bytes(&self)-> Vec<u8>;
-    fn hash(&self)->Vec<u8>{
+    fn hash(&self)->Hash{
         crypto_hash::digest(crypto_hash::Algorithm::SHA256,&self.bytes())
     }
 }
