5b5c88d3d991399bc0321b4aeb87d8a5431f6f1b
fadime <fadime.ozdemir11@gmail.com>
2022-09-18 17:13:37 +0300

    verify blockchains

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use blockchainlib::*;
 
 fn main () {
- let mut block = Block::new(0,0,vec![0;32], 0, "Genesis block".to_owned(),0x000fffffffffffffffffffffffffffff );
- 
- block.hash = block.hash();
- 
- println!("{:?}", &block);
- 
- block.mine();
- 
- println!("{:?}", &block);
+ let difficulty = 0x000fffffffffffffffffffffffffffff;
 
+ let mut block = Block::new(0,now(),vec![0;32], 0, "Genesis block".to_owned(),difficulty );
+ 
+ block.mine(); 
+ println!("Mined genesis block {:?}", &block);
+ 
+ let mut last_hash = block.hash.clone();
+
+ let mut blockchain = Blockchain {
+    blocks: vec![block], 
+ };
+ println!("verify: {}",&blockchain.verify() );
+
+ for i in 1..=10 {
+    let mut block = Block::new(i,now(),last_hash, 0, "Another block".to_owned(),difficulty );
+ 
+    block.mine(); 
+    println!("Mined block {:?}", &block);
+    
+    last_hash = block.hash.clone();
+
+    blockchain.blocks.push(block);
+    println!("verify: {}",&blockchain.verify() );
+ }
 
 }
